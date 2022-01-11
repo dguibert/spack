3ba4c47dea16e6411f3a49338f07ee730eb2458b
David Guibert <david.guibert@gmail.com>
2022-01-11 18:16:54 +0100

    add loadme.sh script

---
+++ loadme.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SPACK_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# remove existing module vars/functions
+unset MODULEPATH MODULEPATH_modshare MODULES_CMD MODULESHOME MODULES_RUN_QUARANTINE
+unset -f module module_raw ml
+
+source $SPACK_ROOT/share/spack/setup-env.sh
+. $(spack location -i lmod arch=$(uname -p))/lmod/lmod/init/profile
+source $SPACK_ROOT/share/spack/setup-env.sh
+#module use $SPACK_ROOT/share/spack/lmod/linux-$(spack arch -o)-$(uname -p)/Core
+module use $SPACK_ROOT/share/spack/lmod/linux-*-*/Core
+module av
+
