fc2a8bca623308debfccc9b16aff3674563d0ecf
David Guibert <david.guibert@gmail.com>
2022-01-17 17:09:35 +0100

    loadme: prepare to be loaded w/o spack

--- loadme.sh
+++ loadme.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 SPACK_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+LOAD_SPACK=${LOAD_SPACK:-true}
 
 # remove existing module vars/functions
 unset MODULEPATH MODULEPATH_modshare MODULES_CMD MODULESHOME MODULES_RUN_QUARANTINE
 unset -f module module_raw ml
 
-source $SPACK_ROOT/share/spack/setup-env.sh
-. $(spack location -i lmod arch=$(uname -p))/lmod/lmod/init/profile
-source $SPACK_ROOT/share/spack/setup-env.sh
-#module use $SPACK_ROOT/share/spack/lmod/linux-$(spack arch -o)-$(uname -p)/Core
+if ${LOAD_SPACK}; then
+  source $SPACK_ROOT/share/spack/setup-env.sh
+  LMOD_PATH=${LMOD_PATH:-$(spack location -i lmod@8.6.5 arch=$(uname -p))}
+  LMOD_PATH=${LMOD_PATH:-$(spack location -i lmod arch=$(uname -p))}
+  source $LMOD_PATH/lmod/lmod/init/profile
+  source $SPACK_ROOT/share/spack/setup-env.sh
+else
+  source $LMOD_PATH/lmod/lmod/init/profile
+fi
 
 module use $SPACK_ROOT/share/spack/lmod/modules
 #os=$(spack arch -o)
