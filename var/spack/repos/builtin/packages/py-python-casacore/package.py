bbf9905724d65a55f139d7686d57e3ecb7490b0d
David Guibert <david.guibert@gmail.com>
2022-02-07 17:59:13 +0100

    py-python-casacore: fix build

--- var/spack/repos/builtin/packages/py-python-casacore/package.py
+++ var/spack/repos/builtin/packages/py-python-casacore/package.py
@@ -14,12 +14,16 @@ class PyPythonCasacore(PythonPackage):
     pypi     = "python-casacore/python-casacore-3.4.0.tar.gz"
 
     version('3.4.0', sha256='f654781292308de70c037981f5f7f5aeb02cf980a6f1367d1c294e7b4fca42ce')
+    version('3.3.1', sha256='c1f196b87ea34f930da6900eebc0a8f39291352d4def6631a2ec148ef5cf083a')
+    version('3.3.0', sha256='e682940c50e6dbeb4e4a3301e3e15cd89774ce6849dfc1b6602b6663c058213b')
+    version('3.2.0', sha256='d48e6202831ef22983961aa14c82294325f71b2fd3e108efaa3c1ca99b7505b5')
+
 
     depends_on('casacore+python', type=('build', 'link'))
     depends_on('boost+python', type=('build', 'link'))
     depends_on('py-six', type=('build', 'run'))
     depends_on('py-numpy', type=('build', 'run'))
-    depends_on('py-argparse', type=('build', 'run'))
+    depends_on('py-argparse', type=('build', 'run'), when='^python@:2.6,3.0:3.1')
     depends_on('py-future', type=('build', 'run'))
 
     # https://github.com/casacore/python-casacore/blob/master/setup.py
@@ -40,5 +44,6 @@ class PyPythonCasacore(PythonPackage):
     def install(self, spec, prefix):
        args = self.build_args()
        python('setup.py', 'build_ext', *args)
+       python('setup.py', 'build')
        python('setup.py', 'install', '--prefix={0}'.format(prefix))
 
