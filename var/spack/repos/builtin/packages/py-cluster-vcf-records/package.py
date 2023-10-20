51ced9e25a0d612eec0e9206d88a9a4f5177c9dd
David Guibert <david.guibert@gmail.com>
2023-10-20 21:59:07 +0200

    cluster_vcf_records: use pypi

--- var/spack/repos/builtin/packages/py-cluster-vcf-records/package.py
+++ var/spack/repos/builtin/packages/py-cluster-vcf-records/package.py
@@ -11,18 +11,19 @@ class PyClusterVcfRecords(PythonPackage):
 
     homepage = "https://github.com/iqbal-lab-org/cluster_vcf_records"
 
-    url = "https://github.com/iqbal-lab-org/cluster_vcf_records/archive/refs/tags/v0.13.2.tar.gz"
+    pypi = "cluster-vcf-records/cluster_vcf_records-0.13.3.tar.gz"
 
-    version("0.13.2", sha256="57cb4e41515e85073d612626b210d4eea3b740491e505dd7af2d2d17727e7963")
-    version("0.13.1", sha256="5cc8768a8fd28fe20052641d6d83cc7599d709608c7b99297f8ef64547266049")
-    version("0.13.0", sha256="f86d87d4941b5c8409bcb653557c9a52086d8e20fd9cc295bd7785a37fc67c6c")
-    version("0.12.4", sha256="785942d5dd2ec5c969a9a3af9704717ed5f9354e7e47682d7c89a84999e652f1")
-    version("0.12.3", sha256="ab42c5bce13c192a12613383c5fc705e81f21c01050d9c5872fcd9774488af9d")
-    version("0.12.2", sha256="f16b643e6a52df88e9d149571fb2560acb0dcf05641cc6d9090ab22285e8713f")
-    version("0.12.1", sha256="8b8dfd6a38c29abe22fe7d0fc9a36ac8c57b8ab4782b8d4afb3e1f8a3470400d")
-    version("0.12.0", sha256="d3aaf7779f77edef89bd701a73ff9c31525ba60a0807539d64300172bdad2919")
-    version("0.11.1", sha256="02d8686150af76a7cb635c7b0149e85381f25b79d225a7052c10f3e9c4c1d601")
-    version("0.11.0", sha256="81bb3892f6383d9d82c6bd6a135f263a5c534034f931ef73693d6b434fa0ca6a")
+    version("0.13.3", sha256="dc8a2f55a4d58159c3152195441c8821abdbe430fc8b529c0d22a1ebda26d58c")
+    version("0.13.2", sha256="ba587de2a5b8330d9cd4ae904f926eb7af6585f340fba45e3c90b37d9a2045b1")
+    version("0.13.1", sha256="4fb16ae3c8495168c911adc51e9d33fad385995ab69e1b41d3ba4ab3e6ab1d35")
+    version("0.13.0", sha256="c863a0415b5a071fafd88b21d6b378fb96cf478ee73a5dafecd5b8f25c6dbb7a")
+    version("0.12.4", sha256="116032bab09c2205ad7fba47e6181b52a383973d6d065e207046de56ce347fa9")
+    version("0.12.3", sha256="45f02153262b1020959180d08893ad50aa80e1b6a72b14a716e6c19bae5cde12")
+    version("0.12.2", sha256="10741b6c0f96aa55415b4125008fdfe14d6084705a19f96a2365806328e7eb65")
+    version("0.12.1", sha256="d8d8d4585a41e375bc53406e6400805c3c1da2a88c96d4d7c00ed55ac1277e88")
+    version("0.12.0", sha256="3871924d34ab1da01e4ffebdc9240679eff7d4e9fd9249b701db5042c4c46432")
+    version("0.11.1", sha256="49140120a495a15cb503c6e0136a2875507b927c6bd11df498be0a100058f5cb")
+    version("0.11.0", sha256="5739559ca9e450e3561a6d44d09c92c5c8fbd877aae80a6ce5cf180a0ef13e97")
 
     depends_on("py-bitarray", type=("build", "run"))
     depends_on("py-fastaq@3.14.0:", type=("build", "run"))
